4fb8f8af90df0285bca26d304a366108b65d9625
james <james@icetech.com.tw>
2024-12-04 15:55:50 +0800

    add android.util.Log

--- src/android/ForegroundService.java
+++ src/android/ForegroundService.java
@@ -14,6 +14,7 @@ import android.os.Build;
 import android.os.IBinder;
 import android.os.PowerManager;
 import android.app.NotificationChannel;
+import android.util.Log;
 
 import org.json.JSONObject;
 
